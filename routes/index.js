512e83caec3061e0e0ea951c3da6aa587cef0a08
Ben Manley <benjamintmanley@gmail.com>
2022-02-23 21:15:54 -0600

    add controller

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ import { Router } from 'express'
 const router = Router()
 
 /* GET home page. */
-router.get('/banana', function(req, res) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Express' })
 })
 
--- routes/turtles.js
+++ routes/turtles.js
@@ -1,10 +1,9 @@
 import { Router } from 'express'
+import * as turtlesCtrl from '../controllers/turtles.js'
 const router = Router()
 
 /* GET users listing. */
-router.get('/', function(req, res) {
-  res.send(`turtles are cool`)
-})
+router.get('/', turtlesCtrl.index)
 
 export {
   router
